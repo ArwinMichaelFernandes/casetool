745000b37fbcfccabb4739de6224be08995c598e
ArwinMichaelFernandes <arvindfernandes00@gmail.com>
2024-01-31 10:33:42 +0530

    commit2

--- src/casetool/vehicle_xxx.java
+++ src/casetool/vehicle_xxx.java
@@ -9,11 +9,14 @@ public class vehicle_xxx {
 		this.brand=brand;
 		this.speed=0;
 	}
-	
+	int decrease_speed(int dec) {
+		this.speed=speed - dec;
+		return dec;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        
 	}
 
 
