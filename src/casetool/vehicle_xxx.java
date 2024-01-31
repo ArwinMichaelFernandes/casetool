d4564e1b75d08e9fb5c63953917bf4b0dbf18c12
ArwinMichaelFernandes <arvindfernandes00@gmail.com>
2024-01-31 10:26:24 +0530

    commit1

---
+++ src/casetool/vehicle_xxx.java
@@ -0,0 +1,20 @@
+package casetool;
+
+public class vehicle_xxx {
+
+	String brand;
+	int speed;
+	
+	vehicle_xxx(String brand){
+		this.brand=brand;
+		this.speed=0;
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
